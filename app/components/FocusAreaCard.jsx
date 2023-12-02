b1ca00cbd3962fc43a6c49ba10c0d44b80efdca9
PromiseUdo <info.promiseudo@gmail.com>
2023-12-02 08:00:19 +0100

    focus area complete

--- app/components/FocusAreaCard.jsx
+++ app/components/FocusAreaCard.jsx
@@ -5,7 +5,7 @@ const FocusAreaCard = ({ icon, title, alt, description }) => {
   return (
     <div className="relative flex flex-col items-center justify-center">
       <div className="bg-[#505ca4] h-[12rem] w-[15rem] rounded-md ">
-        <div className="flex flex-col items-center justify-center gap-4 relative bottom-4 right-4 bg-[#c5c5c5] h-[12rem] w-[15rem] rounded-md">
+        <div className="flex flex-col items-center justify-center gap-4 relative bottom-4 right-4 bg-[#c1c1d1] h-[12rem] w-[15rem] rounded-md">
           <Image src={icon} alt={alt} height={65} width={65} />
           <div className="px-2">
             <h3 className="text-[#333366] tracking-wide text-center font-semibold">
